cb7723a332d3b444e4b0f632e1620e292dd3c063
EgorKr08 <Kryukovegor08@gmail.com>
2024-03-18 20:44:26 +0300

    !

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,27 +1,27 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-    int count1 = 0, low = 0, high = size - 1;
+    int count = 0, low = 0, high = size - 1;
     while (low <= high) {
-        int mid = low + (high - low) / 2;
-        if (arr[mid] == value) {
-            count1++;
-            int iLow = mid - 1;
-            while (iLow >= 0 && arr[iLow] == value) {
-                count1++;
-                iLow--;
-            }
-            int high2 = mid + 1;
-            while (high2 < size && arr[high2] == value) {
-                count1++;
-                high2++;
-            }
-            return count1;
-        } else if (arr[mid] < value) {
+        int mid = (low + high) / 2;
+        if (arr[mid] < value) {
             low = mid + 1;
+        } else if (arr[mid] > value) {
+            high = mid - 1;
         } else {
-            high = mid + 1;
+            count++;
+            int iPerem = mid - 1;
+            mid++;
+            while (arr[mid] == value) {
+                count++;
+                mid++;
+            }
+            while (arr[iPerem] == value) {
+                count++;
+                iPerem--;
+            }
+            break;
         }
     }
-    return 0;
+    return count;
 }
