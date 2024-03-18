73bee43ae4915bb422b1df10eb2433c7439e2ddd
EgorKr08 <Kryukovegor08@gmail.com>
2024-03-18 20:32:27 +0300

    !

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,27 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int count1 = 0, low = 0, high = size - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == value) {
+            count1++;
+            int iLow = mid - 1;
+            while (iLow >= 0 && arr[iLow] == value) {
+                count1++;
+                iLow--;
+            }
+            int high2 = mid + 1;
+            while (high2 < size && arr[high2] == value) {
+                count1++;
+                high2++;
+            }
+            return count1;
+        } else if (arr[mid] < value) {
+            low = mid + 1;
+        } else {
+            high = mid + 1;
+        }
+    }
+    return 0;
 }
